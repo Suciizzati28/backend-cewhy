33e8bdc94fcd8511cfcaec7e6f8d0da2701d38c2
Suciizzati28 <suci_izzati28@student.uns.ac.id>
2023-01-09 23:14:34 +0700

    create cat controller

---
+++ app/Http/Controllers/CatController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cat;
+// use App\Http\Requests\StoreCatRequest;
+// use App\Http\Requests\UpdateCatRequest;
+use Illuminate\Http\Request;
+
+class CatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cat = Cat::all();
+        return response()->json($cat);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'breed' => 'required',
+            'age' => 'required',
+        ]);
+
+        $cat = Cat::create($request->all());
+        return response()->json("Cat added successfully");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $cat = Cat::where('id', $id)->get();
+        return response()->json($cat);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cat $cat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'breed' => 'required',
+            'age' => 'required',
+        ]);
+
+        Cat::where('id', $id)->update($request->all());
+        return response()->json("Cat updated successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Cat::where('id', $id)->delete();
+        return response()->json("Cat deleted successfully");
+    }
+}
